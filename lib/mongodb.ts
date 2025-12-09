d84c4ebdbae917d860780a3c5dbf4ebab70b2c19
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-12-09 12:36:54 +0530

    Refactor MongoDB connection management
    - Updated environment variable name from MONGO_URI to MONGODB_URI for consistency.
    - Improved error handling for missing MONGODB_URI.
    - Streamlined Mongoose connection caching logic and formatting for better readability.
    - Ensured proper TypeScript type definitions for MongooseCache.

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,52 +1,50 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from 'mongoose';
 
-//  Define the connection cache type
 type MongooseCache = {
-    conn : typeof mongoose | null;
-    promise : Promise < typeof mongoose > | null;
-}
-
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
 
 declare global {
-    var mongoose : MongooseCache | undefined
+  var mongoose: MongooseCache | undefined;
 }
 
-const MONGODB_URL = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if(!MONGODB_URL){
-    throw new Error(
-        "Please define the MongoDB_URI Environment variable inside .env local"
-    );
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
 }
-
-let cached : MongooseCache = global.mongoose || { conn : null, promise : null };
-
-if(!global.mongoose){
-    global.mongoose = cached;
-}
-
-
-async function connectDB() : Promise < typeof mongoose > {
-    if(cached.conn){
-        return cached.conn;
-    }
-
-    if(!cached.promise){
-        const options = {
-            bufferCommands : false,
-        }
-
-        cached.promise = mongoose.connect(MONGODB_URL!, options).then((mongoose) => {
-            return mongoose;
-        })
-    }
-    try{
-        cached.conn = await cached.promise;
-    }catch(err){
-        cached.promise = null;
-        throw err;
-    }
+async function connectDB(): Promise<typeof mongoose> {
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    if (!MONGODB_URI) {
+      throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local'
+      );
+    }
+    const options = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI!, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
 }
 
 export default connectDB;
